Tasssadar/device_asus_grouper

Use LZMA compression for recovery's ramdisk

BoardConfigCommon.mk
@@ -124,6 +124,7 @@ TW_BRIGHTNESS_PATH := /sys/devices/platform/pwm-backlight/backlight/pwm-backligh
 TW_MAX_BRIGHTNESS := 255
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 HAVE_SELINUX := true
+BOARD_CUSTOM_BOOTIMG_MK := device/asus/grouper/custombootimg.mk
 
 #MultiROM config. MultiROM also uses parts of TWRP config
 MR_INPUT_TYPE := type_b

andi34/device_samsung_espressowifi

Change-Id: I5a27136c973d6641264249a7fb209848456bb283

BoardConfigCommon.mk
@@ -127,6 +127,8 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 TARGET_RECOVERY_FSTAB := device/samsung/p3100/rootdir/fstab.espresso
 RECOVERY_FSTAB_VERSION := 2
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/p3100/custombootimg.mk
+TARGET_NOT_USE_GZIP_RECOVERY : = true
 
 # TWRP
 TW_THEME := landscape_mdpi