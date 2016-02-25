SlimRoms/device_samsung_p3100

DeviceSettings: Add "About Tab"
Thanks to Max Weninger for helping me getting this work

Change-Id: Icb31303d650eb9531d4d12dfd4089c68c053c261

DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -56,6 +56,8 @@ public class DeviceSettings extends FragmentActivity {
         mTabsAdapter = new TabsAdapter(this, mViewPager);
         mTabsAdapter.addTab(bar.newTab().setText(R.string.category_dock_title),
                 DockFragmentActivity.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText(R.string.action_about),
+                AboutActivity.class, null);
 
         if (savedInstanceState != null) {
             bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));

andi34/device_samsung_espressowifi

DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -56,6 +56,8 @@ public class DeviceSettings extends FragmentActivity {
         mTabsAdapter = new TabsAdapter(this, mViewPager);
         mTabsAdapter.addTab(bar.newTab().setText(R.string.category_dock_title),
                 DockFragmentActivity.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText(R.string.about_action),
+                AboutActivity.class, null);
 
         if (savedInstanceState != null) {
             bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));