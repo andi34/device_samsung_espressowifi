5eb8c67b680376aace0faf83a1fab6cc74b6a512
Andreas Blaesius <skate4life@gmx.de>
2016-07-02 14:04:29 +0200

    More cleanup, reorder files and sync with AOSP
    - move Slim specific defines to a seperate BoardConfigSlim.mk
    
    Change-Id: I1b00e4bef03f4553cc62dc1f5f25a417ffb00866

---
+++ BoardConfigSlim.mk
@@ -0,0 +1,8 @@
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+
+# F2FS filesystem
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Recovery
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
