5cc8af99610a8bffd7a5fa66915e0b83ff93dd15
Andreas Blaesius <skate4life@gmx.de>
2016-10-29 16:39:13 +0200

    sepolicy: move custom policy into own subdir
    Change-Id: I1fe5e299159d206c9206452cfc490e81c2cc21b9

--- BoardConfigSlim.mk
+++ BoardConfigSlim.mk
@@ -6,3 +6,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
 TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/espressowifi/sepolicy-custom
