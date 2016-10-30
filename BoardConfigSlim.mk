ee769bfa43607cbb66d0d46bbbc8e809f02cb903
Andreas Blaesius <skate4life@gmx.de>
2016-10-30 17:16:24 +0100

    Restore TWRP config
    - got removed by accident on commit dc477ff793cdecf9d380ac813d2af817d70c7f7d
      ( sepolicy: move custom policy into own subdir )
    
    Change-Id: I88384b899198c982bc5b224581bbb1b5a334de40
    Signed-off-by: Andreas Blaesius <skate4life@gmx.de>

--- BoardConfigSlim.mk
+++ BoardConfigSlim.mk
@@ -15,6 +15,25 @@ BOARD_CHARGER_SHOW_PERCENTAGE := true
 # Recovery
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/f_mass_storage/lun0/file"
 TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/espressowifi/custombootimg.mk
+TARGET_NOT_USE_GZIP_RECOVERY_RAMDISK := true
+
+# TWRP
+TW_THEME := landscape_hdpi
+HAVE_SELINUX := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_FLASH_FROM_STORAGE := true
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 255
+TW_NO_CPU_TEMP := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
